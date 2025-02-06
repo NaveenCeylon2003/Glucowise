6b5b2826a5541473756dc3e2242f0285e545293b
Naveen Pathberiya <naveenpathberiya@gmail.com>
2025-02-06 19:38:16 +0530

    Firebase and http package set up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:g21285878naveen/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Sugartracking());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
